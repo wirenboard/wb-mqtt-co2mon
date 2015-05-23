46a0004c7ba93f93758018623ceb2ab31d6ac588
Evgeny Boger <boger@contactless.ru>
2015-05-23 23:36:02 +0300

    Add MQTT driver

--- README.md
+++ README.md
@@ -1,43 +1,15 @@
 # Software for CO2 Monitor
 
-## Usage
 
-    sudo cp -iv dbusrules/io.github.dmage.CO2Mon.conf /etc/dbus-1/system.d/
+MQTT driver (Wiren Board conventions).
+Based on https://github.com/dmage/co2mon
 
-    mkdir build
-    cd build
-    cmake ..
-    make
 
-    cd ../graph/
-    make graph.rrd
-    ./update_graph.sh >/dev/null &
-    xdg-open index.html &
-    ./update_data.sh
-
-## D-Bus Signals
-
-    dbus-monitor --system \
-        "type='signal',interface='io.github.dmage.CO2Mon',member='NewValue'"
-
-## D-Bus Methods
-
-    dbus-send --system \
-        --dest=io.github.dmage.CO2Mon \
-        --type=method_call \
-        --print-reply \
-        /io/github/dmage/CO2Mon \
-        io.github.dmage.CO2Mon.GetTemperature
-
-    dbus-send --system \
-        --dest=io.github.dmage.CO2Mon \
-        --type=method_call \
-        --print-reply \
-        /io/github/dmage/CO2Mon \
-        io.github.dmage.CO2Mon.GetCO2
 
 ## See also
 
+  * [Original driver](https://github.com/dmage/co2mon)
+  * [Wiren Board MQTT Conventions](https://github.com/contactless/homeui/blob/contactless/conventions.md)
   * [ZyAura ZG01C Module Manual](http://www.zyaura.com/support/manual/pdf/ZyAura_CO2_Monitor_ZG01C_Module_ApplicationNote_141120.pdf)
   * [RevSpace CO2 Meter Hacking](https://revspace.nl/CO2MeterHacking)
   * [Photos of the device and the circuit board](http://habrahabr.ru/company/masterkit/blog/248403/)
---
+++ wb-mqtt-co2mon/src/main.c
@@ -0,0 +1,224 @@
+/*
+ * co2mon - programming interface to CO2 sensor.
+ * Copyright (C) 2015  Oleg Bulatov <oleg@bulatov.me>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+#include <getopt.h>
+
+#include "co2mon.h"
+#include <mosquitto.h>
+
+
+#define CODE_TEMP 0x42
+#define CODE_CO2 0x50
+#define CODE_HUMIDITY 0x41
+
+uint16_t co2mon_data[256];
+
+struct mosquitto *mosq = NULL;
+
+static double decode_temperature(uint16_t w)
+{
+    return (double)w * 0.0625 - 273.15;
+}
+
+
+static void device_loop(co2mon_device dev)
+{
+    co2mon_magic_table_t magic_table = {0};
+    co2mon_data_t result;
+	char buffer[15];
+
+
+    if (!co2mon_send_magic_table(dev, magic_table)) {
+        fprintf(stderr, "Unable to send magic table to CO2 device\n");
+        return;
+    }
+
+    printf("Sending values to MQTT...\n");
+
+    while (1) {
+        int r = co2mon_read_data(dev, magic_table, result);
+        if (r == LIBUSB_ERROR_NO_DEVICE) {
+            fprintf(stderr, "Device has been disconnected\n");
+            break;
+        } else if (r <= 0) {
+            continue;
+        }
+
+        if (result[4] != 0x0d) {
+            fprintf(stderr, "Unexpected data from device (data[4] = %02hhx, await 0x0d)\n", result[4]);
+            continue;
+        }
+
+        unsigned char r0, r1, r2, r3, checksum;
+        r0 = result[0];
+        r1 = result[1];
+        r2 = result[2];
+        r3 = result[3];
+        checksum = r0 + r1 + r2;
+        if (checksum != r3) {
+            fprintf(stderr, "checksum error (%02hhx, await %02hhx)\n", checksum, r3);
+            continue;
+        }
+
+        uint16_t w = (result[1] << 8) + result[2];
+
+        switch (r0) {
+	        case CODE_TEMP:
+				snprintf(buffer, 15, "%2.1f", decode_temperature(w));
+	            mosquitto_publish(mosq, NULL, "/devices/co2mon/controls/temperature", strlen(buffer), buffer, 2, true);
+	            break;
+	        case CODE_CO2:
+				snprintf(buffer, 15, "%d", w);
+	            mosquitto_publish(mosq, NULL, "/devices/co2mon/controls/co2", strlen(buffer), buffer, 2, true);
+	            break;
+	        //~ case CODE_HUMIDITY:
+				//~ if (w != 0) {
+					//~ snprintf(buffer, 15, "%2.2f", ((float) w) / 10000);
+		            //~ mosquitto_publish(mosq, NULL, "/devices/co2mon/controls/humidity", strlen(buffer), buffer, 2, true);
+				//~ }
+	            //~ break;
+        }
+    }
+}
+
+void monitor_loop()
+{
+    bool show_no_device = true;
+    while (1) {
+        co2mon_device dev = co2mon_open_device();
+        if (dev == NULL) {
+            if (show_no_device) {
+                fprintf(stderr, "Unable to open CO2 device\n");
+                show_no_device = false;
+            }
+        } else {
+            show_no_device = true;
+
+            char path[16];
+            if (co2mon_device_path(dev, path, 16)) {
+                printf("Path: %s\n", path);
+            } else {
+                printf("Path: (error)\n");
+            }
+
+            device_loop(dev);
+
+            co2mon_close_device(dev);
+        }
+        sleep(1);
+    }
+    return ;
+}
+
+
+void publish_mqtt_meta()
+{
+	char* str;
+
+	str = "temperature";
+	mosquitto_publish(mosq, NULL, "/devices/co2mon/controls/temperature/meta/type", strlen(str), str , 2, true);
+
+	str = "concentration";
+	mosquitto_publish(mosq, NULL, "/devices/co2mon/controls/co2/meta/type", strlen(str), str , 2, true);
+
+	str = "CO2 Monitor";
+	mosquitto_publish(mosq, NULL, "/devices/co2mon/meta/name", strlen(str), str , 2, true);
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+    int rc;
+    int c;
+
+	int port = 1883;
+	int tmp;
+	char * endptr = 0;
+	char * host = "127.0.0.1";
+
+
+    while ( (c = getopt(argc, argv, "h:p:")) != -1) {
+        switch (c) {
+        case 'p':
+            tmp = strtol(optarg, &endptr, 0);
+            if (*endptr == '\0') {
+				port = tmp;
+			} else {
+				fprintf(stderr, "Warning: Cannot convert -p argument to integer, ignored\n");
+			}
+
+            break;
+        case 'h':
+            host = optarg;
+            break;
+        case '?':
+            break;
+        default:
+            printf ("?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+
+
+    rc = co2mon_init();
+    if (rc < 0) {
+        return rc;
+    }
+
+	mosquitto_lib_init();
+
+	mosq = mosquitto_new(NULL, true, NULL);
+	if (!mosq) {
+		switch (errno) {
+			case ENOMEM:
+				fprintf(stderr, "Error: Out of memory.\n");
+				break;
+			case EINVAL:
+				fprintf(stderr, "Error: Invalid id and/or clean_session.\n");
+				break;
+		}
+
+		mosquitto_lib_cleanup();
+		return 1;
+	}
+
+	rc = mosquitto_connect(mosq, host, port, 5);
+	if (rc) {
+		fprintf(stderr, "Error: Cannot connect to MQTT broker\n");
+		return rc;
+	}
+
+
+	publish_mqtt_meta();
+
+	mosquitto_loop_start(mosq);
+	monitor_loop();
+
+	mosquitto_destroy(mosq);
+	mosquitto_lib_cleanup();
+
+    co2mon_exit();
+    return 0;
+}
